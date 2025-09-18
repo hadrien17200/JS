ff977835f995171e9196148cb1a51a2296e3bf69
hadrien17200 <hadrien17200@gmail.com>
2025-09-18 11:17:25 +0200

    Les boucles

---
+++ Les boucles/scriptTP-11.js
@@ -0,0 +1,17 @@
+let nbtotale=0;
+
+while(i<=100){
+    nbtotale+=1;
+    i++;
+    alert(`La somme des nombres est $(nbtotale)`);
+}
+
+let nbchoisi=prompt("Choisissez un nombre entre 1 et 100");
+
+    let nb=0;
+    let j=0;
+do{
+    nb+=1;
+    j++;
+    alert(`La somme des nombres est $(nb)`);
+}while(nb =!nbchoisi)
---
+++ Les boucles/scriptTP-12.js
@@ -0,0 +1,18 @@
+let tableau = ["Coldplay","TheWeeknd","Imagine Dragons","Ed Sheeran","Dua Lipa","Maroon 5","Ariana Grande","Bruno Mars","Lewis Capaldi","Shawn Mendes"];
+let i = 0;
+/*while(i < 8){
+    console.log(tableau[i]);
+    i++;
+}
+
+for(let j = 0; j < 8; j++){
+    console.log(tableau[j]);
+}
+
+for(let k = 0; k < tableau.length; k++){
+    console.log(tableau[k]);
+} */
+
+tableau.forEach((el)=>{
+    console.log(el);
+});
